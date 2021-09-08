cdb15c75b0cab47174d247716f5647fab36a8e9d
unknown <przetacznikp@gmail.com>
2021-09-08 22:54:35 +0200

    basic Login layout

--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -1,8 +1,9 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, TextInput, Pressable } from "react-native";
 import { RootParamList } from "../Routes";
 import tw from "tailwind-react-native-classnames";
+import { inputStyle } from "../styles";
 
 type LoginScreenNavProps = NativeStackNavigationProp<RootParamList, "Login">;
 
@@ -11,15 +12,20 @@ interface LoginProps {
 }
 
 export const Login: React.FC<LoginProps> = ({ navigation }) => {
+  const handleSubmit = (): void => {
+    console.log("submit");
+  };
+
   return (
     <View style={tw.style("flex", "items-center", "justify-center", "h-full")}>
-      <Text>I am login screen</Text>
-      <Button
-        title="go to register"
-        onPress={() => {
-          navigation.navigate("Register");
-        }}
-      ></Button>
+      <Text style={tw.style("m-8", "text-4xl")}>Swifoo</Text>
+      <TextInput style={inputStyle} placeholder="email..." />
+      <TextInput style={inputStyle} placeholder="password..." />
+      <View style={tw.style("flex", "flex-row", "items-end")}>
+        <Pressable style={tw.style("bg-black", "px-4", "py-2", "rounded")} onPress={handleSubmit}>
+          <Text style={tw.style("text-white")}>Submit</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
---
+++ src/styles.tsx
@@ -0,0 +1,3 @@
+import tw from "tailwind-react-native-classnames";
+
+export const inputStyle = tw.style("border-b", "p-2", "w-4/5", "mb-2");
