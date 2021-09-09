3312671bf6af4cae4317b34e390d4417bc5e94d0
unknown <przetacznikp@gmail.com>
2021-09-09 20:21:19 +0200

    login screen update

--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -21,8 +21,15 @@ export const Login: React.FC<LoginProps> = ({ navigation }) => {
       <Text style={tw.style("m-8", "text-4xl")}>Swifoo</Text>
       <TextInput style={inputStyle} placeholder="email..." />
       <TextInput style={inputStyle} placeholder="password..." />
-      <View style={tw.style("flex", "flex-row", "items-end")}>
-        <Pressable style={tw.style("bg-black", "px-4", "py-2", "rounded")} onPress={handleSubmit}>
+      <View style={tw.style("flex", "flex-row", "justify-end", "w-4/5", "items-center")}>
+        <Pressable style={tw.style("mr-4")} onPress={() => navigation.push("Register")}>
+          <Text>sign up</Text>
+        </Pressable>
+        <Pressable
+          style={tw.style("bg-black", "px-4", "py-2", "rounded")}
+          onPress={handleSubmit}
+          android_disableSound={false}
+        >
           <Text style={tw.style("text-white")}>Submit</Text>
         </Pressable>
       </View>
